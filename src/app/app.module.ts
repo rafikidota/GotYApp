be43857487668e5ce8f0abd0533e9896a9f34993
rafikidota <lesmes240698@gmail.com>
2022-09-23 16:31:17 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GotyComponent } from './pages/goty/goty.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GotyComponent
   ],
   imports: [
     BrowserModule,
