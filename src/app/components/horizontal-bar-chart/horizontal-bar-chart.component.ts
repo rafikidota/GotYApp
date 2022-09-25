c304bb9204608cda8130998c22f4d78606125394
rafikidota <lesmes240698@gmail.com>
2022-09-24 21:02:52 -0400

    random results

--- src/app/components/horizontal-bar-chart/horizontal-bar-chart.component.ts
+++ src/app/components/horizontal-bar-chart/horizontal-bar-chart.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Result } from 'src/app/interfaces/result';
 
 @Component({
   selector: 'app-horizontal-bar-chart',
   templateUrl: './horizontal-bar-chart.component.html',
   styleUrls: ['./horizontal-bar-chart.component.css']
 })
-export class HorizontalBarChartComponent implements OnInit {
+export class HorizontalBarChartComponent implements OnInit, OnDestroy {
 
-  results: any[] = [
+  results: Result[] = [
     {
       "name": "Dota 2",
       "value": 33
@@ -38,20 +39,43 @@ export class HorizontalBarChartComponent implements OnInit {
 
   colorScheme = 'nightLights';
 
-  constructor() { }
+  interval: any;
 
-  ngOnInit(): void { }
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.updateChart();
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  updateChart() {
+
+    this.interval = setInterval(() => {
+      console.log('tick');
+      const newResults: Result[] = [...this.results];
+      newResults.forEach(result => {
+        result.value = Math.round(Math.random() * 500);
+      });
+      this.results = newResults;
+    }, 3000);
+
+  }
+
 
   onSelect(data: any): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+    // console.log('Item clicked', JSON.parse(JSON.stringify(data)));
   }
 
   onActivate(data: any): void {
-    console.log('Activate', JSON.parse(JSON.stringify(data)));
+    // console.log('Activate', JSON.parse(JSON.stringify(data)));
   }
 
   onDeactivate(data: any): void {
-    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+    // console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 }
