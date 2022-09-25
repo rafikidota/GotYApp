b1e3d8e964e04e5c1783d7c11afae09dc2e13eed
rafikidota <lesmes240698@gmail.com>
2022-09-24 20:31:22 -0400

    ngx-charts-bar-horizontal

--- src/app/components/horizontal-bar-chart/horizontal-bar-chart.component.ts
+++ src/app/components/horizontal-bar-chart/horizontal-bar-chart.component.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HorizontalBarChartComponent implements OnInit {
 
+  results: any[] = [
+    {
+      "name": "Dota 2",
+      "value": 33
+    },
+    {
+      "name": "LoL",
+      "value": 20
+    },
+    {
+      "name": "Call of Duty",
+      "value": 25
+    },
+    {
+      "name": "Battlefield",
+      "value": 21
+    }
+  ];
+
+  // options
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  yAxisLabel: string = 'Games';
+  showYAxisLabel: boolean = true;
+  xAxisLabel: string = 'Votes';
+
+  colorScheme = 'nightLights';
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 }
