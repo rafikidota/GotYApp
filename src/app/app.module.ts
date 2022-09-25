1290196d37e338bfae72e8a9c4c68ee9b077ed40
rafikidota <lesmes240698@gmail.com>
2022-09-24 20:33:38 -0400

    some modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ComponentsModule } from './components/components.module';
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { GotyComponent } from './pages/goty/goty.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    GotyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
